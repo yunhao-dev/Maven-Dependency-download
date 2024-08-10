d4a59f1c889a89275722044cebbd8321ce9adc72
23849 <2384982531@qq.com>
2024-08-11 02:28:56 +0800

    Run success

---
+++ src/main/java/com/wild/maven/analyzer/PomEditorProvider.java
@@ -0,0 +1,61 @@
+package com.wild.maven.analyzer;
+
+import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.fileEditor.FileEditor;
+import com.intellij.openapi.fileEditor.FileEditorPolicy;
+import com.intellij.openapi.fileEditor.FileEditorProvider;
+import com.intellij.openapi.project.DumbAware;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.idea.maven.project.MavenProject;
+import org.jetbrains.idea.maven.project.MavenProjectsManager;
+
+import static org.jetbrains.idea.maven.utils.MavenUtil.isPotentialPomFile;
+
+
+/**
+ * @description:
+ * @Author: yunhao_dev
+ * @Date: 2024/8/10 20:15
+ */
+public class PomEditorProvider implements FileEditorProvider, DumbAware {
+    private static final Logger log = Logger.getInstance(PomEditorProvider.class);
+    @Override
+    public boolean accept(@NotNull Project project, @NotNull VirtualFile file) {
+        return isPomFile(project,file);
+    }
+
+    private boolean isPomFile(Project project, VirtualFile file) {
+        String name = file.getName();
+        if(!isPotentialPomFile(name)){
+            return false;
+        }
+        MavenProjectsManager instance = MavenProjectsManager.getInstance(project);
+        if(instance == null){
+            return false;
+        }
+        MavenProject mavenProject = instance.findProject(file);
+        if(mavenProject == null){
+            return false;
+        }
+        return mavenProject.getFile().equals(file);
+
+    }
+
+    @Override
+    public @NotNull FileEditor createEditor(@NotNull Project project, @NotNull VirtualFile file) {
+        return null;
+    }
+
+    @Override
+    public @NotNull @NonNls String getEditorTypeId() {
+        return null;
+    }
+
+    @Override
+    public @NotNull FileEditorPolicy getPolicy() {
+        return null;
+    }
+}
